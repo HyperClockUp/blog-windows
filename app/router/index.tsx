ed547109c17ed2aeec03f49076796d40a98d6773
chengfx <cfx_software@163.com>
2022-03-06 23:31:45 +0800

    feat:[] add tips for mobile user

--- app/router/index.tsx
+++ app/router/index.tsx
@@ -1,32 +1,39 @@
-import React from 'react';
-import { RouterConfig } from '../types/Route';
-import LoginPage from '../pages/login';
-import MainPage from '../pages/main';
-import TestPage from '../pages/test';
+import React from "react";
+import { RouterConfig } from "../types/Route";
+import LoginPage from "../pages/login";
+import MainPage from "../pages/main";
+import TestPage from "../pages/test";
+import MobilePage from "../pages/mobile";
 
 export enum ROUTES {
-  LOGIN = 'LOGIN',
-  MAIN = 'MAIN',
-  TEST = 'TEST',
+  LOGIN = "LOGIN",
+  MAIN = "MAIN",
+  TEST = "TEST",
+  MOBILE = "MOBILE",
 }
 
 const RouteList: Record<ROUTES, RouterConfig> = {
   [ROUTES.LOGIN]: {
-    path: '/login',
+    path: "/login",
     element: <LoginPage />,
     auth: false,
   },
   [ROUTES.MAIN]: {
-    path: '/',
+    path: "/",
     element: <MainPage />,
     index: true,
     auth: true,
   },
   [ROUTES.TEST]: {
-    path: '/test',
+    path: "/test",
     element: <TestPage />,
     auth: false,
-  }
+  },
+  [ROUTES.MOBILE]: {
+    path: "/mobile",
+    element: <MobilePage />,
+    auth: false,
+  },
 };
 
 export default RouteList;
